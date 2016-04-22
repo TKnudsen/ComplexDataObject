f73d6254b8f00e4f6f60448ae98566858faf3337
Robert Heimbach <robert.heimbach@gmail.com>
2016-04-22 13:04:54 +0200

    Added 3 PreprocessingRoutines and some classes used by those.
    1st) RemovingValues can remove a given list of values.
    2nd) ListConverter converts the value stores as an itemization x,y,z,...
    to a List Object.
    3nd) DummyVariableConverter can convert an attribute which stores its
    values in List Objects to a Set of Dummy Variables - one for each unique
    value.

---
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/preprocessing/ListConverter.java
@@ -0,0 +1,40 @@
+package com.github.TKnudsen.ComplexDataObject.preprocessing;
+
+import com.github.TKnudsen.ComplexDataObject.data.ComplexDataContainer;
+import com.github.TKnudsen.ComplexDataObject.data.ComplexDataObject;
+import com.github.TKnudsen.ComplexDataObject.preprocessing.utility.IItemSplitter;
+
+
+/**
+ * Used to split each value for a given attribute of each {@link ComplexDataObject}  
+ * in the {@link ComplexDataContainer}, using a routine implementing {@link IItemSplitter}.
+ * 
+ * @author Robert Heimbach
+ *
+ */
+public class ListConverter implements IPreprocessingRoutine {
+
+	private String attribute;
+	private IItemSplitter splitter;
+
+	public ListConverter(String attribute, IItemSplitter splitter) {
+		this.attribute = attribute;
+		this.splitter = splitter;
+	}
+
+	@Override
+	public void process(ComplexDataContainer container) {
+		
+		for(ComplexDataObject complexDataObject : container){
+			
+			// Load the old value
+			Object oldValue = complexDataObject.get(attribute);
+			
+			// Split the old value and store if not null
+			if(oldValue != null) {
+				
+				complexDataObject.add(attribute, splitter.split(oldValue));
+			}
+		}
+	}
+}
