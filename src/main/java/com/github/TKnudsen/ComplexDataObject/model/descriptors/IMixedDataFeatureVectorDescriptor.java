2faa7a201fac36349df84a6388e5aff424886e43
jubernar <juergen.bernard@igd.fraunhofer.de>
2016-07-15 20:25:13 +0200

    header added

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/descriptors/IMixedDataFeatureVectorDescriptor.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/descriptors/IMixedDataFeatureVectorDescriptor.java
@@ -2,6 +2,23 @@ package com.github.TKnudsen.ComplexDataObject.model.descriptors;
 
 import com.github.TKnudsen.ComplexDataObject.data.features.mixedData.MixedDataFeatureVector;
 
+/**
+ * <p>
+ * Title: IMixedDataFeatureVectorDescriptor
+ * </p>
+ * 
+ * <p>
+ * Description: Basic Interface to transform real-world data (represented as a
+ * ComplexDataObject) into the mixed data feature space.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2016
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
 public interface IMixedDataFeatureVectorDescriptor extends IDescriptor<Object, MixedDataFeatureVector> {
 
 }
--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/descriptors/INumericFeatureVectorDescriptor.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/descriptors/INumericFeatureVectorDescriptor.java
@@ -2,6 +2,23 @@ package com.github.TKnudsen.ComplexDataObject.model.descriptors;
 
 import com.github.TKnudsen.ComplexDataObject.data.features.numericalData.NumericalFeatureVector;
 
+/**
+ * <p>
+ * Title: INumericFeatureVectorDescriptor
+ * </p>
+ * 
+ * <p>
+ * Description: Basic Interface to transform real-world data (represented as a
+ * ComplexDataObject) into numerical feature spaces.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2016
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
 public interface INumericFeatureVectorDescriptor extends IDescriptor<Double, NumericalFeatureVector> {
 
 }
