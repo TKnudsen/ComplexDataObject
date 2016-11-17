e76d66e23941bc094037300a671fabe238755232
jubernar <juergen.bernard@igd.fraunhofer.de>
2016-11-17 18:44:38 +0100

    FeatureVectorTools

--- src/main/java/com/github/TKnudsen/ComplexDataObject/data/features/FeatureVectorTools.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/data/features/FeatureVectorTools.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 public class FeatureVectorTools {
 
-	public static void addClassAttribute(List<AbstractFeatureVector<?, Feature<?>>> features, List<Object> labels, String classAttribute) {
+	public static void addClassAttribute(List<AbstractFeatureVector<?, Feature<?>>> features, List<String> labels, String classAttribute) {
 		for (int i = 0; i < features.size(); i++)
 			features.get(i).add(classAttribute, labels.get(i));
+	}
+
+	public static void addNumericAttribute(List<AbstractFeatureVector<?, Feature<?>>> features, List<Double> labels, String attributeName) {
+		for (int i = 0; i < features.size(); i++)
+			features.get(i).add(attributeName, labels.get(i));
 	}
 }
