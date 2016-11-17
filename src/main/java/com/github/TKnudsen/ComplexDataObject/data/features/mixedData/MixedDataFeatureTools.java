e429814a558eb2dd72cae80fb13649199791b7df
jubernar <juergen.bernard@igd.fraunhofer.de>
2016-11-17 18:47:20 +0100

    MixedDataFeatureVector

--- src/main/java/com/github/TKnudsen/ComplexDataObject/data/features/mixedData/MixedDataFeatureTools.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/data/features/mixedData/MixedDataFeatureTools.java
@@ -1,5 +1,7 @@
 package com.github.TKnudsen.ComplexDataObject.data.features.mixedData;
 
+import java.util.List;
+
 import com.github.TKnudsen.ComplexDataObject.data.features.FeatureType;
 
 public class MixedDataFeatureTools {
@@ -16,5 +18,15 @@ public class MixedDataFeatureTools {
 		else if (feature instanceof Character)
 			return FeatureType.STRING;
 		throw new IllegalArgumentException("MixedDataFeatureTools.guessFeatureType: undefined object type");
+	}
+
+	public static void addClassAttribute(List<MixedDataFeatureVector> featureVectors, List<String> labels, String classAttribute) {
+		for (int i = 0; i < featureVectors.size(); i++)
+			featureVectors.get(i).add(classAttribute, labels.get(i));
+	}
+
+	public static void addNumericAttribute(List<MixedDataFeatureVector> features, List<Double> labels, String attributeName) {
+		for (int i = 0; i < features.size(); i++)
+			features.get(i).add(attributeName, labels.get(i));
 	}
 }
