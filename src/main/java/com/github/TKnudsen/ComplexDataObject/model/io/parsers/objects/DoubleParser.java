8dac0fd079a60ca4240924745683111a51df4a12
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2017-05-02 13:24:00 +0200

    double parser

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/io/parsers/objects/DoubleParser.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/io/parsers/objects/DoubleParser.java
@@ -1,5 +1,9 @@
 package com.github.TKnudsen.ComplexDataObject.model.io.parsers.objects;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
 /**
  * <p>
  * Title: DoubleParser
@@ -26,15 +30,41 @@ public class DoubleParser implements IObjectParser<Double> {
 		if (stringValue.endsWith(";"))
 			stringValue = stringValue.substring(0, stringValue.length() - 1);
 
-		stringValue = stringValue.trim();
-		stringValue = stringValue.replace(",", ".");
 		stringValue = stringValue.replace("%", "");
 		if (stringValue.contains(" "))
 			stringValue = stringValue.substring(0, stringValue.indexOf(" "));
+
+		if (isDotForThousandsNotation(stringValue)) {
+			NumberFormat nf = NumberFormat.getInstance(Locale.GERMAN);
+			try {
+				Number number = nf.parse(stringValue);
+				if (number != null)
+					return number.doubleValue();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+
+		stringValue = stringValue.trim();
+		stringValue = stringValue.replace(",", ".");
 		try {
 			return Double.parseDouble(stringValue);
 		} catch (Exception e) {
 			return Double.NaN;
 		}
+	}
+
+	private boolean isDotForThousandsNotation(String string) {
+		if (string == null)
+			return false;
+
+		int dot = string.indexOf(".");
+		int comma = string.indexOf(",");
+
+		if (dot > -1 && comma > -1)
+			if (dot < comma)
+				return true;
+
+		return false;
 	}
 }
