be9076e770b51f80a5c7e893a2d1277b27d494e3
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2017-06-06 01:16:33 +0200

    BufferedImageTools

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/tools/BufferedImageTools.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/tools/BufferedImageTools.java
@@ -1,6 +1,9 @@
 package com.github.TKnudsen.ComplexDataObject.model.tools;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,7 +19,7 @@ import java.awt.image.BufferedImage;
  * </p>
  *
  * @author Juergen Bernard
- * @version 1.00
+ * @version 1.02
  */
 public class BufferedImageTools {
 
@@ -33,5 +36,37 @@ public class BufferedImageTools {
 		int B = (color >>> 0) & 0xFF;
 		double luminance = (0.2126 * R + 0.7152 * G + 0.0722 * B) / 255.0;
 		return luminance;
+	}
+
+	/**
+	 * Gathers the Color of a given pixel in a given BufferedImage.
+	 * 
+	 * @param bi
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public static Color getColor(BufferedImage bi, int x, int y) {
+		int rgb = bi.getRGB(x, y);
+		Color color = new Color(rgb, true);
+		return color;
+	}
+
+	/**
+	 * gathers the colors of all pixels from a given BufferedImage
+	 * 
+	 * @param bufferedImage
+	 * @return
+	 */
+	public static List<Color> getColors(BufferedImage bufferedImage) {
+		if (bufferedImage == null)
+			return null;
+
+		List<Color> colors = new ArrayList<>();
+		for (int x = 0; x < bufferedImage.getWidth(); x++)
+			for (int y = 0; y < bufferedImage.getHeight(); y++)
+				colors.add(getColor(bufferedImage, x, y));
+
+		return colors;
 	}
 }
