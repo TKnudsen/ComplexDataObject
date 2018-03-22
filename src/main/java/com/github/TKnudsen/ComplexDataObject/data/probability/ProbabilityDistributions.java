87a3e867c90be90909bd27c6a0ee1f42240c56ad
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-03-22 11:08:31 +0100

    ProbabilityDistributions

---
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/data/probability/ProbabilityDistributions.java
@@ -0,0 +1,22 @@
+package com.github.TKnudsen.ComplexDataObject.data.probability;
+
+/**
+ * <p>
+ * Title: ProbabilityDistributions
+ * </p>
+ * 
+ * <p>
+ * Description: utility class for ProbabilityDistribution objects. Smallest
+ * margin, etc.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2016-2018
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
+public class ProbabilityDistributions {
+
+}
