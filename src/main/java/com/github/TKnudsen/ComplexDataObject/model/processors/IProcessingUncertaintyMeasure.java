705ab87372e42be62f7ad4e5f2df216697e793af
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-09-04 20:20:20 +0200

    uncertainty modeling

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/processors/IProcessingUncertaintyMeasure.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/processors/IProcessingUncertaintyMeasure.java
@@ -2,7 +2,30 @@ package com.github.TKnudsen.ComplexDataObject.model.processors;
 
 import com.github.TKnudsen.ComplexDataObject.data.uncertainty.IUncertainty;
 
+/**
+ * <p>
+ * Title: IProcessingUncertaintyMeasure
+ * </p>
+ * 
+ * <p>
+ * Description: Baseline behavior of a data processing routine.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2011-2018
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.02
+ */
 public interface IProcessingUncertaintyMeasure<D, U extends IUncertainty<?>> {
 
-	public void calculateUncertaintyMeasure();
+	/**
+	 * 
+	 * @param originalData
+	 * @param processedData
+	 * @return
+	 */
+	public abstract void calculateUncertainty(D originalData, D processedData);
+
 }
--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/processors/IUncertainDataProcessor.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/processors/IUncertainDataProcessor.java
@@ -2,7 +2,23 @@ package com.github.TKnudsen.ComplexDataObject.model.processors;
 
 import com.github.TKnudsen.ComplexDataObject.data.uncertainty.IUncertainty;
 
+/**
+ * @deprecated has not proven to be useful. better try to split processors and
+ *             measures. refactoring needed.
+ * @author jubernar
+ *
+ * @param <D>
+ * @param <U>
+ */
 public interface IUncertainDataProcessor<D, U extends IUncertainty<?>> {
 
+	/**
+	 * @deprecated has not proven to be useful. better try to split processors and
+	 *             measures. refactoring needed.
+	 * 
+	 * @param originalData
+	 * @param processedData
+	 * @return
+	 */
 	public IProcessingUncertaintyMeasure<D, U> getUncertaintyMeasure(D originalData, D processedData);
 }
