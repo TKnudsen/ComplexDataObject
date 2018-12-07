7d7f29dcac682018cea90b76bf873c61e00f9b35
TKnudsen <juergen.bernard@gris.tu-darmstadt.de>
2018-12-07 14:58:11 +0100

    added a long weighting kernel

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/weighting/Long/ILongWeightingKernel.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/weighting/Long/ILongWeightingKernel.java
@@ -2,6 +2,6 @@ package com.github.TKnudsen.ComplexDataObject.model.weighting.Long;
 
 import com.github.TKnudsen.ComplexDataObject.model.weighting.IWeightingKernel;
 
-public interface ITemporalWeightingKernel extends IWeightingKernel<Long> {
+public interface ILongWeightingKernel extends IWeightingKernel<Long> {
 
 }
---
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/weighting/Long/LinearLongWeightingKernel.java
@@ -0,0 +1,78 @@
+package com.github.TKnudsen.ComplexDataObject.model.weighting.Long;
+
+/**
+ * 
+ * ComplexDataObject
+ *
+ * Copyright: (c) 2016-2018 Juergen Bernard,
+ * https://github.com/TKnudsen/ComplexDataObject<br>
+ * <br>
+ * 
+ * Linear kernel applied on long distances.
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * 
+ * @version 1.01
+ */
+public class LinearLongWeightingKernel implements ILongWeightingKernel {
+
+	private Long reference;
+	private Long interval;
+
+	/**
+	 * for serialization
+	 */
+	@SuppressWarnings("unused")
+	private LinearLongWeightingKernel() {
+		this(10L);
+	}
+
+	public LinearLongWeightingKernel(Long interval) {
+		this.interval = interval;
+	}
+
+	@Override
+	public Long getInterval() {
+		return interval;
+	}
+
+	@Override
+	public void setInterval(Long t) {
+		this.interval = t;
+	}
+
+	@Override
+	public double getWeight(Long t) {
+		if (Math.abs(reference - t) > interval)
+			return 0.0;
+
+		if (interval == 0)
+			return 1.0;
+
+		return 1.0 - (Math.abs(reference - t) / (double) interval);
+	}
+
+	@Override
+	public Long getReference() {
+		return reference;
+	}
+
+	@Override
+	public void setReference(Long t) {
+		this.reference = t;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof LinearLongWeightingKernel))
+			return false;
+
+		LinearLongWeightingKernel other = (LinearLongWeightingKernel) o;
+
+		return other.interval == interval && other.reference == reference;
+	}
+
+}
