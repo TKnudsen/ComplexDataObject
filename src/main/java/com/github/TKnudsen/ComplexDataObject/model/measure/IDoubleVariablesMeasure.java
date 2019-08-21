026281c14edf47d28a4511721f532dabf6101def
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2019-08-20 18:01:35 -0700

    IDoubleVariablesMeasure

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/measure/IDoubleVariablesMeasure.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/measure/IDoubleVariablesMeasure.java
@@ -2,7 +2,7 @@ package com.github.TKnudsen.ComplexDataObject.model.measure;
 
 import java.util.function.IntToDoubleFunction;
 
-public interface IDoubleVariableMeasure {
+public interface IDoubleVariablesMeasure {
 
 	double compute(IntToDoubleFunction x, IntToDoubleFunction y, int size);
 }
