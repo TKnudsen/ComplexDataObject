62534ac8935142d3ddb83e471ebadbc79a032fef
TKnudsen <mail@juergen-bernard.de>
2021-03-23 16:11:00 +0100

    jackson databind deprecation fix

--- src/main/java/com/github/TKnudsen/ComplexDataObject/model/io/json/ObjectMapperFactory.java
+++ src/main/java/com/github/TKnudsen/ComplexDataObject/model/io/json/ObjectMapperFactory.java
@@ -1,10 +1,16 @@
 package com.github.TKnudsen.ComplexDataObject.model.io.json;
 
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Value;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 
 public class ObjectMapperFactory {
 
@@ -19,8 +25,13 @@ public class ObjectMapperFactory {
 
 	private static void initComplexDataObjectObjectMapper() {
 		complexDataObjectObjectMapper = new ObjectMapper();
-		complexDataObjectObjectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-		complexDataObjectObjectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
+		// complexDataObjectObjectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		complexDataObjectObjectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+				DefaultTyping.NON_FINAL);
+//		complexDataObjectObjectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
+		complexDataObjectObjectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		complexDataObjectObjectMapper.configOverride(Map.class)
+				.setInclude(Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL));
 		complexDataObjectObjectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 		complexDataObjectObjectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
 		complexDataObjectObjectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
@@ -29,7 +40,8 @@ public class ObjectMapperFactory {
 
 	public static ObjectMapper createComplexDataObjectObjectMapper(boolean ignoreUnknownProperties) {
 		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, DefaultTyping.NON_FINAL);
 		objectMapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
 		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 		objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
